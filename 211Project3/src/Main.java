ee00f944fbc27bcf5d91d229c8734d1b4775acae
Ravid Rahman <ravidrahman@gmail.com>
2019-11-13 18:21:01 -0500

    PieChart initialized

--- 211Project3/src/Main.java
+++ 211Project3/src/Main.java
@@ -1,21 +1,52 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("PieChart");
+        Canvas canvas = new Canvas(900, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        BorderPane root = new BorderPane(canvas);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
+        Label text = new Label("Enter the number of characters to be displayed on the pie chart");
+        TextField inputBar = new TextField("");
+        Button button = new Button("Submit");
+        inputBar.setMaxWidth(100);
+        root.setAlignment(text, Pos.TOP_LEFT);
+        root.setAlignment(inputBar, );
+        root.setTop(text);
+        root.setLeft(inputBar);
+        root.setLeft(button);
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                int numToShow = Integer.parseInt(inputBar.getText());
+                HistogramAlphaBet frequency = new HistogramAlphaBet();
+                PieChart chart = new PieChart(canvas.getWidth()/2, canvas.getHeight()/2, canvas.getHeight(), canvas.getWidth(), frequency);
+                chart.draw(gc, numToShow);
+            }
+        };
+        button.setOnAction(event);
     }
 
 
--- 211Project3/src/PieChart.java
+++ 211Project3/src/PieChart.java
@@ -1,2 +1,29 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.Random;
+
 public class PieChart {
+    private double x;
+    private double y;
+    private double height;
+    private double width;
+    private  HistogramAlphaBet drawSource;
+
+    PieChart(double x, double y, double height, double width, HistogramAlphaBet drawSource){
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        this.drawSource = drawSource;
+    }
+
+    public void draw(GraphicsContext gc, int numArcs){
+
+    }
+
+    public Color getRandColor(){
+        Random rand = new Random();
+        return Color.rgb(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
+    }
 }
